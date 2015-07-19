23a6cd040f53d0e045cc6e038d760103a4d4df38
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-07-19 11:59:48 +0300

    Refactoring

--- src/scripts/pages/Index/Index.js
+++ src/scripts/pages/Index/Index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 
-import Header from './Header';
-import Footer from './Footer';
-
 import './Index.css';
 
 export default class Index {
   render() {
     return (
-      <DocumentTitle title='Index'>
+      <DocumentTitle title='Lens ­ smart profiling tool for Rails apps.'>
         <section className='page page--index'>
-          <Header />
-          <Footer />
+          <header className='promo'>
+            <div className='promo__content'>
+              <h1 className='promo__title'>Lens</h1>
+              <p className='promo__desc'>
+                Smart profiling tool for Rails apps.
+              </p>
+            </div>
+            <a href='#' className='button button--sign-in'>
+              <span className='icon icon--github'></span>
+              Sign in with GitHub
+            </a>
+          </header>
         </section>
       </DocumentTitle>
     );
